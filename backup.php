0d191b5d6982388c633edb6c4ee79ae432700186
DESKTOP-T066UKA\nabil <yoga@intermediatech.id>
2023-05-04 03:20:18 +0700

    change url

--- backup.php
+++ backup.php
@@ -19,7 +19,7 @@ $uid = $common->authenticate($db, $username, $password, array());
 // connect db
 $servername = "localhost";
 $usernameDb = "root";
-$passwordDB = "nabil";
+$passwordDB = "";
 $dbname = "bridge_odoo";
 
 // Create connection
@@ -85,54 +85,54 @@ $dataSheet = [];
 				);
 			//$dataSheet[$noK]['item'] = [];
 			$nonya = -1;
-			foreach ($surveiItemPerQuetsion as $surveiItemPerQuetsionKey => $surveiItemPerQuetsionValue) 
-			{
-				$surveyId = 77;
-				$answerValue = intval($surveiItemPerQuetsionValue['display_name']);
-				$answerType = $surveiItemPerQuetsionValue['answer_type'];
-				$participantId = $arrQtsKey;
-				$qtsId = $surveiItemPerQuetsionValue['id'];
-				$createdAt = $surveiItemPerQuetsionValue['write_date'];
-				//check dulu
-				//echo print_r($surveiItemPerQuetsionValue);
-				$cek = "SELECT * FROM survey_answers WHERE 
-						survey_id='$surveyId' 
-						AND 
-						quetsion_id='$qtsId'
-						AND 
-						participant_id='$participantId'";
-				$result = $conn->query($cek);
-				if($result->num_rows <= 0)
-				{
-					if(isset($surveiItemPerQuetsionValue['matrix_row_id']))
-					{
-						if(is_array($surveiItemPerQuetsionValue['matrix_row_id']))
-						{
-							$nonya++;
-							$answerValue = $surveiItemPerQuetsionValue['suggested_answer_id'][1];
-							$dataSheet[$noK]['items'][$nonya] = $answerValue;
-							$sql = "INSERT INTO survey_answers (survey_id, quetsion_id, type, value, participant_id, created_at)
-									VALUES ('$surveyId'
-											,'$qtsId'
-											,'$answerType'
-											,'$answerValue'
-											,'$participantId'
-											,'$createdAt')";
-							if ($conn->query($sql) === TRUE) 
-							{
-								$total++;
-							}
-						}
-					} 
-				}
-			}
+			//echo print_r($surveiItemPerQuetsion);
+			// foreach ($surveiItemPerQuetsion as $surveiItemPerQuetsionKey => $surveiItemPerQuetsionValue) 
+			// {
+			// 	$surveyId = 77;
+			// 	$answerValue = intval($surveiItemPerQuetsionValue['display_name']);
+			// 	$answerType = $surveiItemPerQuetsionValue['answer_type'];
+			// 	$participantId = $arrQtsKey;
+			// 	$qtsId = $surveiItemPerQuetsionValue['id'];
+			// 	$createdAt = $surveiItemPerQuetsionValue['write_date'];
+			// 	//check dulu
+
+			// 	$cek = "SELECT * FROM survey_answers WHERE 
+			// 			survey_id='$surveyId' 
+			// 			AND 
+			// 			quetsion_id='$qtsId'
+			// 			AND 
+			// 			participant_id='$participantId'";
+			// 	$result = $conn->query($cek);
+			// 	if($result->num_rows <= 0)
+			// 	{
+			// 		if(isset($surveiItemPerQuetsionValue['matrix_row_id']))
+			// 		{
+			// 			if(is_array($surveiItemPerQuetsionValue['matrix_row_id']))
+			// 			{
+			// 				$nonya++;
+			// 				$dataSheet[$noK]['items'][$nonya] = $answerValue;
+			// 				$sql = "INSERT INTO survey_answers (survey_id, quetsion_id, type, value, participant_id, created_at)
+			// 						VALUES ('$surveyId'
+			// 								,'$qtsId'
+			// 								,'$answerType'
+			// 								,'$answerValue'
+			// 								,'$participantId'
+			// 								,'$createdAt')";
+			// 				if ($conn->query($sql) === TRUE) 
+			// 				{
+			// 					$total++;
+			// 				}
+			// 			}
+			// 		} 
+			// 	}
+			// }
 		}
 
 
-if($total > 0)
-{
-	updateSheet($dataSheet,$jml['jml']);
-}
+// if($total > 0)
+// {
+// 	updateSheet($dataSheet,$jml['jml']);
+// }
 $conn->close();
 echo print_r($dataSheet);
 
