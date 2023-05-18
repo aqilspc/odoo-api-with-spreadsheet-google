68f373a827fe9957fd5619ef0f54cdedcbbac262
DESKTOP-T066UKA\nabil <yoga@intermediatech.id>
2023-05-18 12:49:41 +0700

    typo jeneng spedset

--- new.php
+++ new.php
@@ -47,120 +47,202 @@ if($latestParticipantId['jml'] > 0)
 			)
 		); 
 	}
-echo json_encode($surveiItemList,true);
-// $arrQts = [];
-// $total = 0;
-// $dataSheet = [];
+//echo json_encode($surveiItemList,true);
 
-// 		foreach ($surveiItemList as $surveiItemListKey => $surveiItemListValue) 
-// 		{
+$arrQts = [];
+$total = 0;
+$dataSheet = [];
 
-// 			$surveiItemDetail = $models->execute_kw($db, $uid, $password
-// 				, 'survey.user_input'
-// 				, 'search_read', 
-// 					array(
-// 						array(
-// 							array('id','=',$surveiItemListValue['id']) // from {1} => object array ['id'] => under looping;
-// 						)
-// 					)
-// 				);
+		foreach ($surveiItemList as $surveiItemListKey => $surveiItemListValue) 
+		{
 
-// 				foreach ($surveiItemDetail[0]['predefined_question_ids'] as $surveiItemDetailValueUiLiKey => $surveiItemDetailValueUiLiValue) 
-// 				{
-// 					$arrQts[$surveiItemListValue['id']][$surveiItemDetailValueUiLiKey] = $surveiItemDetailValueUiLiValue;
-// 				}
-// 		}
+			$surveiItemDetail = $models->execute_kw($db, $uid, $password
+				, 'survey.user_input'
+				, 'search_read', 
+					array(
+						array(
+							array('id','=',$surveiItemListValue['id']) // from {1} => object array ['id'] => under looping;
+						)
+					)
+				);
 
-// 		$noK = -1;
-// 		$arrNotScrore = ['Nama','NIM','Mata Kuliah','Nama Dosen','Berikan pesan dan saran kepada dosen yang bersangkutan'];
-// 		$arrJk = ['Jenis Kelamin','Program Studi'];
-// 		$arrNum = ['Kode Mata Kuliah'];
-// 		foreach ($arrQts as $arrQtsKey => $arrQtsValue) 
-// 		{
-// 			$noK++;
-// 			$dataSheet[$noK]['items'] = null;
-// 			$surveiItemPerQuetsion = $models->execute_kw($db, $uid, $password
-// 			, 'survey.user_input.line'
-// 			, 'search_read', 
-// 				array(
-// 					array(
-// 						array('survey_id','=',66),
-// 						array('user_input_id','=',$arrQtsKey),
-// 						array('question_id','=',$arrQtsValue) // from {2} => object array ['predefined_question_ids'] => under looping
-// 						)
-// 					)
-// 				);
-// 			//break;
-// 			//echo json_encode($surveiItemPerQuetsion);
-// 			$dataSheet[$noK]['item'] = [];
-// 			$nonya = -1;
-// 			foreach ($surveiItemPerQuetsion as $surveiItemPerQuetsionKey => $surveiItemPerQuetsionValue) 
-// 			{
-// 				$surveyId = 66;
-// 				$answerValue = intval($surveiItemPerQuetsionValue['display_name']);
-// 				$answerType = $surveiItemPerQuetsionValue['answer_type'];
-// 				$participantId = $arrQtsKey;
-// 				$qtsId = $surveiItemPerQuetsionValue['id'];
-// 				$createdAt = $surveiItemPerQuetsionValue['write_date'];
-// 				$cek = "SELECT * FROM survey_answers WHERE 
-// 						survey_id='$surveyId' 
-// 						AND 
-// 						quetsion_id='$qtsId'
-// 						AND 
-// 						participant_id='$participantId'";
-// 				$result = $conn->query($cek);
-// 				if($result->num_rows <= 0)
-// 				{
-// 					$nonya++;
-// 					if(isset($surveiItemPerQuetsionValue['question_id'][1]))
-// 					{
-// 						$typeQuetsion = $surveiItemPerQuetsionValue['question_id'][1];
-// 						//$typeQuetsion = explode(' ', $typeQuetsion);
-// 						if(isset($typeQuetsion))
-// 						{
-// 							if(in_array($typeQuetsion, $arrNotScrore))
-// 							{
-// 								$answerValue = $surveiItemPerQuetsionValue['value_char_box'];
-// 								$answerValue = str_replace("'", ' ', $answerValue);
-// 							}elseif(in_array($typeQuetsion, $arrJk)){
-// 								if(isset($surveiItemPerQuetsionValue['suggested_answer_id'][1]))
-// 								{
-// 									$answerValue = $surveiItemPerQuetsionValue['suggested_answer_id'][1];
-// 									$answerValue = str_replace("'", ' ', $answerValue);
-// 								}
+				foreach ($surveiItemDetail[0]['predefined_question_ids'] as $surveiItemDetailValueUiLiKey => $surveiItemDetailValueUiLiValue) 
+				{
+					$arrQts[$surveiItemListValue['id']][$surveiItemDetailValueUiLiKey] = $surveiItemDetailValueUiLiValue;
+				}
+		}
+
+		$noK = -1;
+		$arrNotScrore = ['Nama','NIM','Mata Kuliah','Nama Dosen','Berikan pesan dan saran kepada dosen yang bersangkutan'];
+		$arrJk = ['Jenis Kelamin','Program Studi'];
+		$arrNum = ['Kode Mata Kuliah'];
+		foreach ($arrQts as $arrQtsKey => $arrQtsValue) 
+		{
+			$noK++;
+			$dataSheet[$noK]['items'] = null;
+			$surveiItemPerQuetsion = $models->execute_kw($db, $uid, $password
+			, 'survey.user_input.line'
+			, 'search_read', 
+				array(
+					array(
+						array('survey_id','=',66),
+						array('user_input_id','=',$arrQtsKey),
+						array('question_id','=',$arrQtsValue) // from {2} => object array ['predefined_question_ids'] => under looping
+						)
+					)
+				);
+			//break;
+			//echo json_encode($surveiItemPerQuetsion);
+			$dataSheet[$noK]['item'] = [];
+			$nonya = -1;
+			foreach ($surveiItemPerQuetsion as $surveiItemPerQuetsionKey => $surveiItemPerQuetsionValue) 
+			{
+				$surveyId = 66;
+				$answerValue = intval($surveiItemPerQuetsionValue['display_name']);
+				$answerType = $surveiItemPerQuetsionValue['answer_type'];
+				$participantId = $arrQtsKey;
+				$qtsId = $surveiItemPerQuetsionValue['id'];
+				$createdAt = $surveiItemPerQuetsionValue['write_date'];
+				$cek = "SELECT * FROM survey_answers WHERE 
+						survey_id='$surveyId' 
+						AND 
+						quetsion_id='$qtsId'
+						AND 
+						participant_id='$participantId'";
+				$result = $conn->query($cek);
+				if($result->num_rows <= 0)
+				{
+					$nonya++;
+					if(isset($surveiItemPerQuetsionValue['question_id'][1]))
+					{
+						$typeQuetsion = $surveiItemPerQuetsionValue['question_id'][1];
+						//$typeQuetsion = explode(' ', $typeQuetsion);
+						if(isset($typeQuetsion))
+						{
+							if(in_array($typeQuetsion, $arrNotScrore))
+							{
+								$answerValue = $surveiItemPerQuetsionValue['value_char_box'];
+								$answerValue = str_replace("'", ' ', $answerValue);
+							}elseif(in_array($typeQuetsion, $arrJk)){
+								if(isset($surveiItemPerQuetsionValue['suggested_answer_id'][1]))
+								{
+									$answerValue = $surveiItemPerQuetsionValue['suggested_answer_id'][1];
+									$answerValue = str_replace("'", ' ', $answerValue);
+								}
 								
 								
-// 							}elseif(in_array($typeQuetsion, $arrNum))
-// 							{
-// 								$answerValue = $surveiItemPerQuetsionValue['value_numerical_box'];
-// 							}else{
-// 								$answerValue = $surveiItemPerQuetsionValue['answer_score'];
-// 							}
-// 							// $qtss = $surveiItemPerQuetsionValue['question_id'][1];
-// 							$dataSheet[$noK]['items'][$nonya] = $answerValue;
-// 							$sql = "INSERT INTO survey_answers (survey_id, quetsion_id, type, value, participant_id, created_at)
-// 									VALUES ('$surveyId'
-// 											,'$qtsId'
-// 											,'$answerType'
-// 											,'$answerValue'
-// 											,'$participantId'
-// 											,'$createdAt')";
-// 							if ($conn->query($sql) === TRUE) 
-// 							{
-// 									$total++;
-// 							}
+							}elseif(in_array($typeQuetsion, $arrNum))
+							{
+								$answerValue = $surveiItemPerQuetsionValue['value_numerical_box'];
+							}else{
+								$answerValue = $surveiItemPerQuetsionValue['answer_score'];
+							}
+							$dataSheet[$noK]['items'][$nonya] = $answerValue;
+							$sql = "INSERT INTO survey_answers (survey_id, quetsion_id, type, value, participant_id, created_at)
+									VALUES ('$surveyId'
+											,'$qtsId'
+											,'$answerType'
+											,'$answerValue'
+											,'$participantId'
+											,'$createdAt')";
+							if ($conn->query($sql) === TRUE) 
+							{
+									$total++;
+							}
 
 						
-// 						}
-// 					}
-// 				}
-// 			}
-// 				// if($total >= 10)
-// 				// {
-// 				// 	break;
-// 				// }
-// 		}
+						}
+					}
+				}
+			}
+		}
+
+
+if($total > 0)
+{
+	$total = 0;
+	$totalQts = 24;
+	$limit = 60;
+	$jml = $conn->query("SELECT COUNT(*) AS jml FROM survey_answers WHERE spreadsheet = 1")->fetch_array();
+
+	//echo $jml['jml']*2;
+
+	$qry = "SELECT *  FROM survey_answers WHERE spreadsheet = 0 GROUP BY participant_id ORDER BY id LIMIT $limit";
+	$query 	= mysqli_query($conn, $qry);
+	$arr = [];
+	while ($row = mysqli_fetch_array($query)) 
+	{
+		$arr[$total] = $row['participant_id'];
+		$total++;
+	}
+	$dataSheet = [];
+	$jalan = 0;
+
+
+	foreach ($arr as $key => $value) 
+	{
+		$qry = "SELECT *  FROM survey_answers WHERE participant_id = $value ORDER BY id";
+		$query 	= mysqli_query($conn, $qry);
+		$titit = 0;
+		while ($row = mysqli_fetch_array($query)) 
+		{
+			$dataSheet[$key]['items'][$titit] = $row['value'];
+			$titit++;
+
+			$id = $row['id'];
+			$sql = "UPDATE survey_answers SET spreadsheet= 1 WHERE id=$id";
+
+			if ($conn->query($sql) === TRUE) {
+				$jalan++;
+			}
+		}
+	}
+
+	$conn->close();
+
+	$jml = $jml['jml'] / 24;
+	if($total > 0) //spreedsheet
+	{
+		if($jml < 3)
+		{
+			 $jml = 0;
+		}
+		updateSheet($dataSheet,$jml);
+	}
+}
 
 echo $total;
+
+function updateSheet($data,$jml)
+	{
+		require_once('vendor/autoload.php');
+		$client = new \Google_Client();
+		$client->setApplicationName('Google Sheets API');
+		$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+		$client->setAccessType('offline');
+		$path = 'survei-kepuasan-mahasiswa-2023-dcc41a29a18f.json'; // credentials.json is the key file we downloaded while setting up our Google Sheets API
+		$client->setAuthConfig($path);
+		$service = new \Google_Service_Sheets($client);
+		$spreadsheetId = '1jVLJNDEkgmVS8ibYJ73zdrCV3nB1weJJT7CvMJwe5sU';
+		$range = 'data'; // here we use the name of the Sheet to get all the rows
+		$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+		$values = $response->getValues();
+		$cell = $jml+2;
+		foreach ($data as $dataKey => $dataValue) 
+		{
+			if(!is_null($dataValue['items']))
+			{
+				$cell++;
+				$updateRow = $dataValue['items'];
+				$rows = [$updateRow];
+				$valueRange = new \Google_Service_Sheets_ValueRange();
+				$valueRange->setValues($rows);
+				$range = 'Data!A'.$cell.''; 
+				$options = ['valueInputOption' => 'USER_ENTERED'];
+				$service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
+			}	
+		}
+	}
+
 
 
